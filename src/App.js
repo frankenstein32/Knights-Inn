ad0b30a16c6bb2e73315c2a580e19916a33e883c
frankenstein32 <saifirashid32@gmail.com>
2020-09-14 19:35:01 +0530

    routes setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom'
 import './App.css';
+
+// Componenets import
+import Error from './pages/Error'
+import Home from './pages/Home'
+import Rooms from './pages/Rooms'
+import SingleRoom from './pages/SingleRoom'
 
 function App() {
   return (
    <div>
-     <h1>Hello world</h1>
+     <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/rooms/" component={Rooms} />
+      <Route exact path="/rooms/:slug" component={SingleRoom} />
+      <Route exact component={Error} />
+     </Switch>
    </div>
   );
 }
